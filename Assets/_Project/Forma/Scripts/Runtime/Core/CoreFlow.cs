228fa951792184b3887b2f7b9eb2f83c6caf0c48
egorio <egor1268@gmail.com>
2025-12-13 13:04:51 +0300

    add CoreScope

---
+++ Assets/_Project/Forma/Scripts/Runtime/Core/CoreFlow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using VContainer.Unity;
+
+namespace Forma.Runtime.Core
+{
+    public class CoreFlow : IStartable
+    {
+        public void Start()
+        {
+            Debug.Log("CoreFlow.Start()");
+        }
+    }
+}
---
+++ Assets/_Project/Forma/Scripts/Runtime/Core/CoreScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Forma.Runtime.Core
+{
+    public class CoreScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterEntryPoint<CoreFlow>();
+        }
+    }
+}
