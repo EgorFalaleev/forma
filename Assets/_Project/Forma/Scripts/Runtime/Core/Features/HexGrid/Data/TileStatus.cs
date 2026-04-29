5fbad9040be86338229b7bff612a0a15547d161b
egorio <egor1268@gmail.com>
2026-04-29 12:19:15 +0300

    add HexOccupancyController

---
+++ Assets/_Project/Forma/Scripts/Runtime/Core/Features/HexGrid/Data/TileStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Forma.Runtime.Core.Features.HexGrid.Data
+{
+    public enum TileStatus
+    {
+        Active,
+        Occupied,
+        Unavailable
+    }
+}
---
+++ Assets/_Project/Forma/Scripts/Runtime/Core/Features/HexGrid/HexOccupancyController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Forma.Runtime.Core.Features.HexGrid.Data;
+
+namespace Forma.Runtime.Core.Features.HexGrid
+{
+    public class HexOccupancyController
+    {
+        readonly HexTileRegistry _hexTileRegistry;
+
+        readonly HashSet<HexCubeCoordinates> _occupiedCells;
+
+        public HexOccupancyController(HexTileRegistry hexTileRegistry)
+        {
+            _hexTileRegistry = hexTileRegistry;
+
+            _occupiedCells = new HashSet<HexCubeCoordinates>
+            {
+                new(0, 0)
+            };
+        }
+
+        public void Occupy(HexCubeCoordinates coordinates)
+        {
+            _occupiedCells.Add(coordinates);
+        }
+
+        public TileStatus GetTileStatus(HexCubeCoordinates coordinates)
+        {
+            if (_occupiedCells.Contains(coordinates))
+                return TileStatus.Occupied;
+
+            HexCubeCoordinates[] tileNeighbours = coordinates.GetNeighbours();
+
+            foreach (HexCubeCoordinates neighbourCoordinates in tileNeighbours)
+            {
+                if (!_hexTileRegistry.Tiles.ContainsKey(neighbourCoordinates))
+                    continue;
+
+                if (_occupiedCells.Contains(neighbourCoordinates))
+                    return TileStatus.Active;
+            }
+
+            return TileStatus.Unavailable;
+        }
+    }
+}
