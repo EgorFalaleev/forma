4d1d56d670ecfc633babb8ad271e9f578297e102
egorio <egor1268@gmail.com>
2026-06-02 15:49:24 +0300

    add game states

--- Assets/_Project/Forma/Scripts/Runtime/Composition/Core/CoreFlow.cs
+++ Assets/_Project/Forma/Scripts/Runtime/Composition/Core/CoreFlow.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using Forma.Runtime.Core.Enemy;
 using Forma.Runtime.Core.Features.Camera;
 using Forma.Runtime.Core.Features.HexGrid;
 using Forma.Runtime.Core.Features.Turret;
+using Forma.Runtime.Core.StateMachine;
+using Forma.Runtime.GameStates;
 using Forma.Runtime.Input;
 using Forma.Runtime.Player;
-using Forma.Runtime.Services.Input;
 using UnityEngine;
 using VContainer.Unity;
 
@@ -17,62 +17,58 @@ namespace Forma.Runtime.Composition.Core
           ITickable,
           IDisposable
     {
-        readonly IEnumerable<BaseInputService> _inputServices;
         readonly EnemyFlow _enemyFlow;
         readonly HexGridFlow _hexGridFlow;
         readonly TurretFlow _turretFlow;
         readonly CameraController _cameraController;
-        readonly PlayerFactory _playerFactory;
-        readonly MoveInputHandler _moveInputHandler;
+        readonly StateMachine _gameStateMachine;
+        readonly GameStatesGraph _gameStatesGraph;
 
-        public CoreFlow(IEnumerable<BaseInputService> inputServices, EnemyFlow enemyFlow,
-            HexGridFlow hexGridFlow, TurretFlow turretFlow,
-            CameraController cameraController, PlayerFactory playerFactory,
+        public CoreFlow(EnemyFlow enemyFlow, HexGridFlow hexGridFlow,
+            TurretFlow turretFlow, CameraController cameraController,
+            PlayerFactory playerFactory, PlayerRepository playerRepository,
             MoveInputHandler moveInputHandler)
         {
-            _inputServices = inputServices;
             _enemyFlow = enemyFlow;
             _hexGridFlow = hexGridFlow;
             _turretFlow = turretFlow;
             _cameraController = cameraController;
-            _playerFactory = playerFactory;
-            _moveInputHandler = moveInputHandler;
+
+            _gameStatesGraph = new GameStatesGraph(
+                playerFactory,
+                playerRepository,
+                moveInputHandler
+            );
+
+            _gameStateMachine = _gameStatesGraph.StateMachine;
         }
 
         public void Start()
         {
             Debug.Log("CoreFlow.Start()");
 
-            _moveInputHandler.Enable();
-            
-            _playerFactory.Create(new Vector3(0f, 1f, 0f));
+            // _enemyFlow.Initialize();
+            // _hexGridFlow.Initialize();
+            // _turretFlow.Initialize();
+            // _cameraController.Initialize();
 
-            _enemyFlow.Initialize();
-            _hexGridFlow.Initialize();
-            _turretFlow.Initialize();
-            _cameraController.Initialize();
-
-            foreach (BaseInputService inputService in _inputServices)
-                inputService.Enable();
+            _gameStatesGraph.EnterInitialState();
         }
 
         public void Tick()
         {
-            _enemyFlow.Tick();
-            _hexGridFlow.Tick();
-            _turretFlow.Tick();
+            // _enemyFlow.Tick();
+            // _hexGridFlow.Tick();
+            // _turretFlow.Tick();
+
+            _gameStateMachine.Tick();
         }
 
         public void Dispose()
         {
-            _moveInputHandler.Disable();
-            
-            foreach (BaseInputService inputService in _inputServices)
-                inputService.Disable();
-
-            _enemyFlow.Dispose();
-            _hexGridFlow.Dispose();
-            _turretFlow.Dispose();
+            // _enemyFlow.Dispose();
+            // _hexGridFlow.Dispose();
+            // _turretFlow.Dispose();
         }
     }
 }
---
+++ Assets/_Project/Forma/Scripts/Runtime/GameStates/StartBattleState.cs
@@ -0,0 +1,35 @@
+﻿using Forma.Runtime.Core.StateMachine.States;
+using Forma.Runtime.Core.StateMachine.Triggers;
+using Forma.Runtime.Player;
+using UnityEngine;
+
+namespace Forma.Runtime.GameStates
+{
+    public class StartBattleState : IState
+    {
+        public ITrigger OnBattleStarted => _onBattleStarted;
+        
+        readonly PlayerFactory _playerFactory;
+        readonly PlayerRepository _playerRepository;
+        readonly Trigger _onBattleStarted;
+
+        public StartBattleState(PlayerFactory playerFactory,
+            PlayerRepository playerRepository)
+        {
+            _playerFactory = playerFactory;
+            _playerRepository = playerRepository;
+
+            _onBattleStarted = new Trigger();
+        }
+
+        public void OnEnter()
+        {
+            Player.Player player = _playerFactory.Create(new Vector3(0f, 1f, 0f));
+            _playerRepository.Register(player);
+            
+            _onBattleStarted.Fire();
+        }
+
+        public void OnExit() { }
+    }
+}
