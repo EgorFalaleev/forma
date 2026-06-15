b8dd1477d3c493b4da6b810a8f78aa793eaa6a34
egorio <Egor1268@gmail.com>
2026-06-15 12:54:32 +0500

    add health

--- Assets/_Project/Forma/Scripts/Runtime/Player/Player.cs
+++ Assets/_Project/Forma/Scripts/Runtime/Player/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Forma.Runtime.Movement;
+using R3;
 using UnityEngine;
 
 namespace Forma.Runtime.Player
@@ -6,13 +7,17 @@ namespace Forma.Runtime.Player
     public class Player : MonoBehaviour
     {
         [SerializeField] RigidbodyMovement _movement;
+        [SerializeField] Health _health;
         [SerializeField] float _speed = 5f;
 
         IMoveInput _moveInput;
+        CompositeDisposable _disposables = new();
 
         public void Construct(IMoveInput moveInput)
         {
             _moveInput = moveInput;
+
+            _health.OnDied.Subscribe(OnDied).AddTo(_disposables);
         }
 
         void FixedUpdate()
@@ -21,7 +26,10 @@ namespace Forma.Runtime.Player
             _movement.Move(delta);
         }
 
+        
+        void OnDied(Unit _)
         {
+            Destroy(gameObject);
         }
     }
 }
