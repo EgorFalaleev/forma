b82368e48c40e1c64a8af8ef263b3c2ea1b0969f
egorio <Egor1268@gmail.com>
2026-06-16 17:37:58 +0500

    add restriction to movement config

--- Assets/_Project/Forma/Scripts/Runtime/Movement/Configs/MovementConfig.cs
+++ Assets/_Project/Forma/Scripts/Runtime/Movement/Configs/MovementConfig.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Forma/Configs/Movement")]
 public class MovementConfig : ScriptableObject
 {
-    [field: SerializeField] public float Speed { get; private set; }
+    [field: Min(0f)] [field: SerializeField] public float Speed { get; private set; }
 }
